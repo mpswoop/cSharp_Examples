4cd7087ff08083f3c51bbb977cdc0789dc5d3487
MP <mp@swoop.pro>
2023-07-04 19:22:15 +0300

    Add Example 006 - dind Max

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 8;
+int c = 2;
+int d = 7;
+int e = 6;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine("max = ");
+Console.WriteLine(max);
