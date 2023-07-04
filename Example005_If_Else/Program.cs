eff010e6c0665f2a36c305ee9518edea4b760b63
MP <mp@swoop.pro>
2023-07-04 19:12:31 +0300

    Example 005 - If / Else

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Представьтесь пожалуйста");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "michael")
+{
+    Console.Write("Ура! Это же ");
+    Console.WriteLine(username);
+    Console.Write("!");
+}
+else
+{
+    Console.Write("Hello ");
+    Console.WriteLine(username);
+}
